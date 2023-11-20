1537c998d0f5834feae0e6f8e9dc3152e7d29e3d
yusei53 <yusei530922@gmail.com>
2023-11-20 20:42:31 +0900

    サイドバーチャンネルコンポーネントを反映

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,30 +1,37 @@
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
 
 const Sidebar = () => {
   return (
-    <div className="Sidebar">
-      <div className="SidebarLeft">
-        <div className="SidebarIcon">
+    <div className="sidebar">
+      <div className="sidebarLeft">
+        <div className="sidebarIcon">
           <img src="./logo192.png" alt="icon" />
         </div>
-        <div className="SidebarIcon">
+        <div className="sidebarIcon">
           <img src="./logo192.png" alt="icon" />
         </div>
       </div>
-      <div className="SidebarRight">
-        <div className="SidebarTop">
+      <div className="sidebarRight">
+        <div className="sidebarTop">
           <h3>Discord</h3>
           <ExpandMoreIcon />
         </div>
-        <div className="SidebarChannels">
-          <div className="SidebarChannelsHeader">
-            <div className="SidebarHeader">
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
               <ExpandMoreIcon />
               <h4>チャンネル</h4>
             </div>
-            <AddIcon className="SidebarAddIcon" />
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
           </div>
         </div>
       </div>
