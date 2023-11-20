8bf21969932cd4a1d5e557c55461bab7cff28a2d
yusei53 <yusei530922@gmail.com>
2023-11-20 20:42:01 +0900

    チャンネルコンポーネント作成

---
+++ src/components/sidebar/SidebarChannel.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SidebarChannel.scss";
+
+const SidebarChannel = () => {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannelHash">#</span>
+        Slack
+      </h4>
+    </div>
+  );
+};
+
+export default SidebarChannel;
