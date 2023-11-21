7a8617356f62483f96dc11756bd2315464d58972
yusei53 <yusei530922@gmail.com>
2023-11-21 19:09:25 +0900

    chatコンポーネントを作成

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,34 @@
+import "./Chat.scss";
+import ChatHeader from "./ChatHeader";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import GifIcon from "@mui/icons-material/Gif";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import ChatMessage from "./ChatMessage";
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      <ChatHeader />
+      <div className="chatMessage">
+        <ChatMessage />
+      </div>
+      <div className="chatInput">
+        <AddCircleIcon />
+        <form>
+          <input type="text" placeholder="Slackへメッセージを送る" />
+          <button type="submit" className="chatInputButton">
+            送信
+          </button>
+        </form>
+        <div className="chatInputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
