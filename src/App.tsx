9d588ec464eb8a6338e6d65a13b07b99382f2d07
yusei53 <yusei530922@gmail.com>
2023-11-21 19:08:59 +0900

    画面を整える

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
+import Chat from "./components/chat/Chat";
 
 function App() {
-  return <Sidebar />;
+  return (
+    <>
+      <div className="App">
+        <Sidebar />
+        <Chat />
+      </div>
+    </>
+  );
 }
 
 export default App;
