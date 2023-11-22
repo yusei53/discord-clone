6fc28e7835531da9b19baeed855169d08b33ebcf
yusei53 <yusei530922@gmail.com>
2023-11-22 15:54:01 +0900

    メッセージコンポーネント作成

---
+++ src/components/chat/ChatMessage.tsx
@@ -0,0 +1,19 @@
+import "./ChatMessage.scss";
+import { Avatar } from "@mui/material";
+
+const ChatMessage = () => {
+  return (
+    <div className="message">
+      <Avatar />
+      <div className="messageInfo">
+        <h4>
+          Yusei
+          <span className="messageTimestamp">2022/12/18</span>
+        </h4>
+        <p>メッセージ本文</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatMessage;
