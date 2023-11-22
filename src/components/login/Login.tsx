37afed62c55c2fbc52b773c715c982a0d604d40a
yusei53 <yusei530922@gmail.com>
2023-11-22 18:36:04 +0900

    google認証を可能にした

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,13 +1,21 @@
 import { Button } from "@mui/material";
 import "./Login.scss";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../firebase";
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
+
   return (
     <div className="login">
       <div className="loginLogo">
         <img src="./discordIcon.png" alt="" />
       </div>
 
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   );
 };
