0e8fc679c8020b61134ef3b7d524699b4767f9e2
yusei53 <yusei530922@gmail.com>
2023-11-22 18:52:28 +0900

    ログインしたユーザー情報をStoreに通知して状態更新させる

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,32 @@ import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
 import Login from "./components/login/Login";
-import { UseAppSelector } from "./app/hooks";
+import { UseAppSelector, useAppDispatch } from "./app/hooks";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
   const user = UseAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayname: loginUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <>
