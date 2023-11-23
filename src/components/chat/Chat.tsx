a9dc07e0b88d1a132ae092fe0928c48da3efde88
yusei53 <yusei530922@gmail.com>
2023-11-23 20:47:03 +0900

    サブコレクションにメッセージ情報を追加する

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -7,13 +7,43 @@ import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import ChatMessage from "./ChatMessage";
 import { UseAppSelector } from "../../app/hooks";
 import React, { useState } from "react";
+import {
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Chat = () => {
   const [inputText, setInputText] = useState<string>("");
   const ChannelName = UseAppSelector((state) => state.channel.channelName);
+  const channelId = UseAppSelector((state) => state.channel.channelId);
+  const user = UseAppSelector((state) => state.user.user);
 
-  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
+
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user,
+      }
+    );
+    console.log(docRef);
   };
 
   return (
