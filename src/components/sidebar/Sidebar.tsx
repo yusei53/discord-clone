849de30565a1b51edffdc3c8f9188fd4989d2ed9
yusei53 <yusei530922@gmail.com>
2023-11-23 13:40:20 +0900

    チャンネルを追加する実装

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,10 +8,20 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import { auth, db } from "../../firebase";
 import { UseAppSelector } from "../../app/hooks";
 import useCollection from "../../hooks/useCollection";
+import { addDoc, collection } from "firebase/firestore";
 
 const Sidebar = () => {
   const user = UseAppSelector((state) => state.user);
   const { documents: channels } = useCollection("channels");
+
+  const addChannel = async () => {
+    let channelName: string | null = prompt("新しいチャンネルを作成します");
+    if (channelName) {
+      await addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      });
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -34,7 +44,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h4>チャンネル</h4>
             </div>
-            <AddIcon className="sidebarAddIcon" />
+            <AddIcon className="sidebarAddIcon" onClick={() => addChannel()} />
           </div>
           <div className="sidebarChannelList">
             {channels.map((channel) => (
